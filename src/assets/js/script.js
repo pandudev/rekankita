eaafcddaeedeed1ae3d61c6d0fd92f061ceb18ab
panduwijayasaputra <panduwijayasaputra@gmail.com>
2020-06-18 02:44:35 +0700

    commit

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,3 +2,31 @@
 window.$ = window.jQuery = require("jquery");
 
 // Script Here
+$(document).ready(function () {
+  // smooth scroll
+  $(".navbar a").click(function (e) {
+    var tujuan = $(this).attr("href");
+    var elemenTujuan = $(tujuan);
+
+    $(document.body).stop().animate(
+      {
+        scrollTop: elemenTujuan.offset().top,
+      },
+      1000,
+      "swing"
+    );
+  });
+
+  // scrolling page
+  $(window).scroll(function () {
+    var wScroll = $(this).scrollTop();
+    var showPoint = $(".show-gotop").offset().top;
+
+    // toggle show gotop button
+    if (wScroll >= showPoint) {
+      $("#goTop").addClass("show");
+    } else {
+      $("#goTop").removeClass("show");
+    }
+  });
+});
